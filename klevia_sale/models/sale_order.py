b9ab889d546b7dbd131b4de15a50c54e91ccba7e
PC LISA <najouachaouch3@gmail.com>
2025-02-18 15:29:04 +0100

    [ADD] sale: PDF preview button

--- klevia_sale/models/sale_order.py
+++ klevia_sale/models/sale_order.py
@@ -7,3 +7,10 @@ class SaleOrder(models.Model):
     include_sow = fields.Boolean(string="Include Statement of Work")
     statement_of_work = fields.Html(string="Statement of Work")
 
+    def action_pdf_preview(self):
+        self.ensure_one()
+        return {
+            'type': 'ir.actions.act_url',
+            'url': "/report/pdf/sale.report_saleorder/%s" % self.id,
+            'target': 'new'
+        }
