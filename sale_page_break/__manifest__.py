8b07a28b8b96d4293ba93ffc8d247d9e3fb68794
PC LISA <najouachaouch3@gmail.com>
2025-02-20 10:53:13 +0100

    [ADD] sale: Boolean flag to insert page break

---
+++ sale_page_break/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Page break',
+    'category': "Sales",
+    'version': "18.0.1.0.2",
+    'license': "OPL-1",
+    'author': "OneSolutions - Najoua Chaouch",
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'views/sale_order.xml',
+        'report/sale_order_templates.xml'
+    ],
+    'installable': True,
+    'application': True,
+
+}
+
