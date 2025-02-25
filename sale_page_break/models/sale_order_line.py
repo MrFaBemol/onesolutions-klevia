7b623e64e37d5c10789922d1e9d44928ff1d184e
Najou <najouachaouch3@gmail.com>
2025-02-25 10:10:36 +0000

    fix an issue in sale-page-break

--- sale_page_break/models/sale_order_line.py
+++ sale_page_break/models/sale_order_line.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    is_checked = fields.Boolean(string='Page break', required=False)
+    is_page_break = fields.Boolean(string='Page break', required=False,store=True)
 
