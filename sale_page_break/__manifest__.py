23ff37d20a3e7a6323cb168f4157393dd8b5ea94
Najou <najouachaouch3@gmail.com>
2025-05-15 17:49:45 +0100

    [FIX] sale_page_break: correct appearance of add_page_break_after field in sections and notes.

--- sale_page_break/__manifest__.py
+++ sale_page_break/__manifest__.py
@@ -1,21 +1,21 @@
 {
     'name': 'Page break',
     'category': "Sales",
-    'version': "18.0.1.0.2",
+    'version': "18.0.1.0.3",
     'license': "OPL-1",
     'author': "OneSolutions - Najoua Chaouch",
     'depends': [
-        'sale'
+        'sale','account'
     ],
     'assets': {
         'web.assets_backend': [
-            'sale/static/src/js/sale_order_line_field/*'
             'sale_page_break/static/src/js/custom_sale_order_line_renderer.js'
+
         ],
     },
     'data': [
         'views/sale_order.xml',
-        'report/sale_order_templates.xml'
+        'report/sale_order_templates.xml',
     ],
     'installable': True,
     'application': True,
--- sale_page_break/static/src/js/custom_sale_order_line_renderer.js
+++ sale_page_break/static/src/js/custom_sale_order_line_renderer.js
@@ -1,21 +1,62 @@
 /** @odoo-module **/
 
 import { registry } from '@web/core/registry';
+import { BooleanField } from '@web/views/fields/boolean/boolean_field';
 import { SaleOrderLineListRenderer } from '@sale/js/sale_order_line_field/sale_order_line_field';
+import {ProductLabelSectionAndNoteOne2Many } from '@account/components/product_label_section_and_note_field/product_label_section_and_note_field';
+
+
 
 export class CustomSaleOrderLineListRenderer extends SaleOrderLineListRenderer {
 
+    PAGE_BREAK_FIELD_NAME = 'add_page_break_after';
+
+
     isCellReadonly(column, record) {
-        if (column.name === 'add_page_break_after') {
+        if (column.name === this.PAGE_BREAK_FIELD_NAME) {
             return false;
         }
-        return super.isCellReadonly(column, record);
+        return super.isCellReadonly(column, record) || (
+            this.isComboItem(record)
+                && ![this.titleField, 'tax_id', 'qty_delivered', this.PAGE_BREAK_FIELD_NAME].includes(column.name)
+        );
+    }
+
+
+    getCellClass(column, record) {
+        const classNames = super.getCellClass(column, record);
+        if (this.isSectionOrNote(record) && column.name === this.PAGE_BREAK_FIELD_NAME) {
+            return classNames.replace('o_hidden', '').trim();
+        }
+        return classNames;
+    }
+
+
+    getColumns(record) {
+        let columns = super.getColumns(record);
+        if (this.isSectionOrNote(record)) {
+            const addPageBreakAfterCol = this.columns.find(col => col.name === this.PAGE_BREAK_FIELD_NAME);
+            if (addPageBreakAfterCol && !columns.find(col => col.name === this.PAGE_BREAK_FIELD_NAME)) {
+                const titleFieldIdx = columns.findIndex(col => col.name === this.titleField);
+
+                if (titleFieldIdx !== -1) {
+                    if (columns[titleFieldIdx].colspan > 1) {
+                        columns[titleFieldIdx].colspan -= 1;
+                    }
+                    columns.splice(titleFieldIdx, 0, addPageBreakAfterCol);
+                } else {
+                    columns.push(addPageBreakAfterCol);
+                }
+            }
+        }
+        return columns;
     }
 }
 
 
+
+
 import { saleOrderLineOne2Many } from '@sale/js/sale_order_line_field/sale_order_line_field';
-import { ProductLabelSectionAndNoteOne2Many } from '@account/components/product_label_section_and_note_field/product_label_section_and_note_field';
 
 export class CustomSaleOrderLineOne2Many extends ProductLabelSectionAndNoteOne2Many {
     static components = {
@@ -24,9 +65,9 @@ export class CustomSaleOrderLineOne2Many extends ProductLabelSectionAndNoteOne2M
     };
 }
 
+
 const customField = {
     ...saleOrderLineOne2Many,
     component: CustomSaleOrderLineOne2Many,
 };
-
 registry.category('fields').add('sol_o2m_custom', customField);
